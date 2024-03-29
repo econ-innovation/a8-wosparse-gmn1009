5208a74a01d0796910a10f200529e43a4d2ac4d1
gmn1009 <78292693+gmn1009@users.noreply.github.com>
2024-03-29 17:05:33 +0800

    Update wosparse.py

--- wosparse.py
+++ wosparse.py
@@ -4,7 +4,7 @@ df = pd.read_csv(filepath, sep='\t')
 papers_basic_info = df[['UT', 'PY', 'SO', 'SN', 'DI', 'VL', 'IS']].copy()
 papers_abstracts = df[['UT', 'AB']].copy()
 papers_titles = df[['UT', 'TI']].copy()
-papers_authors = df[['UT', 'AU']].copy()  # 这里只是简单复制了包含作者信息的列
+papers_authors = df[['UT', 'AU']].copy()  
 papers_basic_info.to_csv('papers_basic_info.csv', index=False)
 papers_abstracts.to_csv('papers_abstracts.csv', index=False)
 papers_titles.to_csv('papers_titles.csv', index=False)
